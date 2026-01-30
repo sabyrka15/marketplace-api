00229fe15d0b0c7c336c3dc0b44d3dddf116d60e
Sabyrka <sabyrka15@gmail.com>
2026-01-30 12:06:33 +0500

    feat: add ProductSeeder to populate initial product data

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Product::create([
+            'name' => 'Product 1',
+            'price' => 100,
+        ]);
+
+        \App\Models\Product::create([
+            'name' => 'Product 2',
+            'price' => 200,
+        ]);
+    }
+}
